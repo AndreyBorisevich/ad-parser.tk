51f3ed807cecc6cb1d61fcc2a23363ba73e3e631
AndreyBorisevich <a.borysevich@pdu.by>
2018-11-05 13:52:20 +0300

    Create project

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=ad_parser',
+    'username' => 'ad_parser',
+    'password' => 'ad_parser_2018',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
